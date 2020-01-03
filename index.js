cfe851a1e662c75fcfa56ef49cc574dd9fbc9957
Alan Krajina <alankrajina88@gmail.com>
2020-01-03 13:56:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Your code here
 function createEmployeeRecord(array){
-
+  let obj = {
+    firstName:  firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: timeInEvents,
+    timeOutEvents: timeOutEvents
+  };
+  // Return it
+  return obj;
 }
