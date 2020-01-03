ba90e7e3a479da16542feadcbc3ff4148a1282b3
Alan Krajina <alankrajina88@gmail.com>
2020-01-03 15:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,5 @@ function hoursWorkedOnDate(record, date) {
 }
 
 function wagesEarnedOnDate(record, date) {
-    let timeIn = record.timeInEvents.find(f => f.date === date)
-    let timeOut = record.timeOutEvents.find(f => f.date === date)
-    return (timeOut.hour - timeIn.hour)/100
+
 }
